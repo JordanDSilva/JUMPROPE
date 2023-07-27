5267b8be8f3ff35aaf9f62d0327c4d38df3e6b39
JordanDSilva <jordan.dsilva@research.uwa.edu.au>
2023-07-27 09:05:10 +0800

    Some additional info in initialise env

--- initialise_env.sh
+++ initialise_env.sh
@@ -1,12 +1,17 @@
-export JUMPROPE_MAST_TOKEN=9999
+export JUMPROPE_MAST_TOKEN=9999 #Only needed for exclusive access data. 
 export JUMPROPE_DOWNLOAD_DIR=''
 export JUMPROPE_CRDS_PATH=''
 export JUMPROPE_CRDS_CONTEXT='jwst_1100.pmap'
 
 
-export JUMPROPE_REF_DIR='/Users/22252335/Desktop/pro' ## I recommend you keep this uncommented. Only add when you are certain the directory structure has been made. The code will auto prompt you to create it on first run.
-export JUMPROPE_RAW_DIR='/Users/222335/Desktop/pro/Imaging'
+export JUMPROPE_REF_DIR='' ## I recommend you keep this uncommented. Only add when you are certain the directory structure has been made. The code will auto prompt you to create it on first run.
+export JUMPROPE_RAW_DIR=''
 export JUMPROPE_do_NIRISS=F
-export JUMPROPE_cores_pro=6
-export JUMPROPE_cores_stack=4
-export JUMPROPE_tasks_stack=1
+export JUMPROPE_cores_pro=6 #Number of cores to use for 1/f, sky subtr, wisp rem
+export JUMPROPE_cores_stack=4 #Number of cores to use internally for propaneWarpStackInVar
+export JUMPROPE_tasks_stack=1 #Number of stacking tasks to run
+
+
+# e.g., If I have 20 cal files to process, I could set cores_pro = 5 and only do 4 lots of computations.
+# I could then set cores_stack = 10 and tasks_stack = 2 to stack two things at once while taking advantage of 10 cores for the WCS warping. 
+# cores_stack * tasks_stack < Number of Cores. Use detectCores() in R or other to check system resources. 
