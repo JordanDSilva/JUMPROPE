cc65043a385e89dc84ee1910de55c5e772f88560
JordanDSilva <jordan.dsilva@research.uwa.edu.au>
2024-11-18 12:27:10 +0800

    Standard query and caljwst - integrated cone search and jwst VID

--- CALIBRATION/caljwst.py
+++ CALIBRATION/caljwst.py
@@ -1,51 +1,49 @@
-###################################################################################################
-# JWST Calibration Pipeline, Takes us from Uncal files to Cal files                                #
-# Need to have JWST calibration pipeline installed. Activate python environment on Simon's machine.#
-###################################################################################################
+#####################################################################
+# JWST Calibration Pipeline, Takes us from Uncal files to Cal files.#
+# Need to have JWST calibration pipeline installed.                 #
+#####################################################################
 
 import numpy as np
 import os
-
-os.environ["CRDS_SERVER_URL"] = "https://jwst-crds.stsci.edu"  # where to download reference files from
-
-from jwst.pipeline import calwebb_detector1  # import detector processor, uncal-->rates
-from jwst.pipeline import calwebb_image2  # import image processor, rates-->cal
-from astroquery.mast import Observations
-from astropy.io import ascii
-
+import pandas as pd
 import glob
 import sys
-import argparse  # to run from command line, run <python3 calwebb.py -h> to display help
+import argparse
+
+os.environ["CRDS_SERVER_URL"] = "https://jwst-crds.stsci.edu"  # where to download reference files from
+from jwst.pipeline import calwebb_detector1  # import detector processor, uncal-->rates
+from jwst.pipeline import calwebb_image2  # import image processor, rates-->cal
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--VISITID",
-                    help = "VISITID. e.g., `2736001001` for SMACS")
-parser.add_argument("--INSTRUMENT",
-                    help = "Instrument/Camera e.g., NIRCAM or MIRI",
-                    default = "NIRCAM")
+parser.add_argument('--RA', type=float, nargs="?",
+                    help='Central right ascension of catalogue (deg)', default=110.8375)
+parser.add_argument('--DEC', type=float, nargs="?", help="Central declination of catalogue (deg)", default=-73.4391)
+parser.add_argument('--RAD', type=float, nargs="?", help="Search radius (deg)", default=1.0)
+
+parser.add_argument('--VISITID', type=str, nargs="+", help="VISIT ID (e.g., 2736 for SMACS)", default = None)
+
 parser.add_argument("--redo",
                     type=bool,
                     help="Should we redo the calibration even if files exist?",
                     default=False)
-parser.add_argument("--max_cores", help="How many cores e.g., 'half'", default="half")
+parser.add_argument("--max_cores", help="How many cores. Must be written as e.g., 'half', 'quarter' or 'all'", default="half")
 args = parser.parse_args()
 
+def run1(input_data, cal_dir):
+    """Process every uncal to cal"""
 
-def run1(input_data):
-    """Process every uncal to cal. These are hard coded for convenience. Users should edit this function directly to make changes as per the recommendations on the JWST Calibration Pipeline documentation."""
-
-    files = input_data
-    detector1 = calwebb_detector1.Detector1Pipeline()
-
-    detector1.output_dir = rates_dir
-    detector1.ipc.skip = False
-
-    detector1.jump.rejection_threshold = 4.0
-    detector1.jump.expand_large_events = True
-
+    # detector1.clean_flicker_noise.fit_method = 'fft'
+    # detector1.jump.rejection_threshold = 4.0
+    # detector1.jump.expand_large_events = True
     # detector1.sat_required_snowball = False
     # detector1.max_jump_to_flag_neighbors = 1
     # detector1.min_jump_to_flag_neighbors = 100000
+
+    # # Make the cal and rates directories
+    os.makedirs(cal_dir, exist_ok=True)
+
+    files = input_data
+    detector1 = calwebb_detector1.Detector1Pipeline()
 
     detector1.jump.maximum_cores = args.max_cores
     detector1.ramp_fit.maximum_cores = args.max_cores
@@ -58,9 +56,62 @@ def run1(input_data):
     image2.resample.skip = True  # we don't need to resample the cals, since they get drizzled later
     image2.run(run_output)
 
+def cal(ref_dir, ra, dec, rad, visitid):
+
+    if visitid is None:
+        print("CALJWST in region")
+        uncal_dir = os.path.join(ref_dir, "JWST",
+                                 str(round(ra, 2)) + "_" + str(round(dec, 2)) + "_" +
+                                 str(round(rad, 2)),
+                                 "UNCAL") + str("/")
+    else:
+        print("CALJWST in region")
+        uncal_dir = os.path.join(ref_dir, str(visitid), "UNCAL") + str("/")
+
+    files = glob.glob(uncal_dir + "/**/*fits", recursive = True)
+    uncal_files = [foo.split("_uncal")[0].split("/")[-1] for foo in files]
+
+    cal_dir = []
+    cal_files = []
+    for i in range(len(files)):
+        getUncalPath = os.path.dirname(files[i])
+        getCalPath = getUncalPath.replace("UNCAL", "CAL")
+        cal_dir.append(getCalPath)
+        getCalFile = os.path.join(getCalPath, uncal_files[i] + "_cal.fits")
+        cal_files.append(getCalFile)
+    cal_files_names = [foo.split("_cal")[0].split("/")[-1] for foo in cal_files]
+
+    ## Read info tables from cone search
+    info_tables = glob.glob(
+        uncal_dir + "*info.csv"
+    )
+    df = pd.concat((pd.read_csv(f) for f in info_tables), ignore_index=True)
+
+    if (len(df) > len(files)):
+        print("Some files appear to be missing in this region \n compared to what was downloaded with cone-search.")
+
+    if args.redo:  ## override previous checks. In case we want to use a new pmap of calibration version for example
+        for cal in cal_files:
+            print("Removing " + str(cal))
+            if os.path.isfile(cal):
+                os.remove(cal)
+        files_redo = files
+    else:
+        files_redo = []
+        for cal in cal_files:
+            if not os.path.isfile(cal):
+                files_redo.append(cal)
+
+    print(files_redo)
+    print("Running " + str(len(files_redo)) + " files")
+
+    if len(files_redo) > 0:
+        for i, ff in enumerate(files_redo):
+            run1(ff, cal_dir[i])
+    else:
+        exit()
 
 if __name__ == "__main__":
-
     JUMPROPE_DOWNLOAD_DIR = os.getenv('JUMPROPE_DOWNLOAD_DIR')
 
     JUMPROPE_CRDS_PATH = os.getenv('JUMPROPE_CRDS_PATH')
@@ -73,88 +124,8 @@ if __name__ == "__main__":
         print("Please set ENV variables. ")
         exit()
 
-    N = 6  # N cores/processes
-
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
-    elif args.VISITID is not None:
-
-        instrument = args.INSTRUMENT
-
-        visitid = args.VISITID
-        PID = str(visitid)[0:4]
-        ref_dir = JUMPROPE_DOWNLOAD_DIR
-
-        uncal_dir = os.path.join(ref_dir, PID, 'UNCAL/', instrument) # folder must  be present in input directory
-        files = glob.glob(uncal_dir + "/*fits")
-        # files = [s for s in files if visitid in s]
-        uncal_files = [foo.split("_uncal")[0].split("/")[-1] for foo in files]
-
-
-        rates_dir = os.path.join(ref_dir, PID, 'RATES/', instrument) # rates directory
-        rates_files = glob.glob(rates_dir + "/*fits")
-        # rates_files = [s for s in rates_files if visitid in s]
-        rates_files_names = [foo.split("_cal")[0].split("/")[-1] for foo in rates_files]
-
-        cal_dir = os.path.join(ref_dir, PID, 'CAL/', instrument) # cal directory
-        cal_files = glob.glob(cal_dir + "/*fits")
-        # cal_files = [s for s in cal_files if visitid in s]
-        cal_files_names = [foo.split("_cal")[0].split("/")[-1] for foo in cal_files]
-
-        # # Make the cal and rates directories
-        os.makedirs(cal_dir, exist_ok=True)
-        os.makedirs(rates_dir, exist_ok=True)
-
-        ## download csv from mast to check expected files and their sizes
-        obs_table = Observations.query_criteria(proposal_id=PID,
-                                                project='JWST',
-                                                dataproduct_type="IMAGE",
-                                                instrument_name=[instrument, instrument+"/IMAGE"])
-        data_products = Observations.get_product_list(obs_table)
-        products_stage2 = Observations.filter_products(data_products,
-                                                       extension="fits",
-                                                       productSubGroupDescription="CAL",
-                                                       )
-        visit_ids = np.array([obs[3:13] for obs in products_stage2["obs_id"]])
-        idx = np.flatnonzero(np.core.defchararray.find(visit_ids, str(visitid)) != -1)
-        df = products_stage2[idx].to_pandas().drop_duplicates("productFilename")
-        # df = dff[dff["dataRights"] == "PUBLIC"]
-        if(len(df) > len(files)):
-            print("Some files appear to be missing in VISIT PROGRAM " + visitid + "\n compared to what I can find on MAST.")
-
-            if sum(df["dataRights"] == "EXCLUSIVE_ACCESS") == len(df) - len(uncal_files):
-                print("Exclusive access files found.")
-                df = df[df["dataRights"] == "PUBLIC"]
-            else:
-                df = df[df["obs_id"].isin(uncal_files)]
-
-        if args.redo:  ## override previous checks. In case we want to use a new pmap of calibration version for example
-            for cal, rate in zip(cal_files, rates_files):
-                print("Removing " + str(cal))
-                os.remove(cal)
-                print("Removing " + str(rate))
-                os.remove(rate)
-            files_redo = files
-        else:
-            files_redo = []
-            if sum(df["obs_id"].isin(cal_files_names)) != len(df["obs_id"]): ## check that all of the expected cal files have been produced
-
-                idx = list(~df["obs_id"].isin(cal_files_names))
-
-                for i,j in enumerate(idx):
-                    if j:
-                        files_redo.append(files[i])
-
-        print("Running " + str(len(files_redo)) + " files")
-        for ff in files_redo:
-            run1(ff)
-
-# I’ve processed all the NIRCam smacs frames with cal-webb. The issue I was having was that two of the dark frames must not have downloaded #properly, so I redownloaded them manually. Process took about ~1 hour with 10 frames running in parallel over 270 frames. These are all with pmap 0953.
-# Data summary:
-# crds (reference files) = 64.13GB
-# uncals = 20.07 GB
-# rates = 58.96GB
-# cals = 31.74 GB
-# There is an option to not save the rate files, but I think Jake does the wisp correction at the rate #level. He also basically runs with #default cal webb parameters.
-# I also ran 2 frames of the IDF with identical settings and they worked fine => it doesn’t #automatically redownload reference files. So it #must be that the darks+flats+etc are pmap dependent.
+    else:
+        cal(JUMPROPE_DOWNLOAD_DIR, args.RA, args.DEC, args.RAD, args.VISITID)
---
+++ CALIBRATION/query.py
@@ -0,0 +1,231 @@
+##Query data from MAST and download a .sh script
+## Execute shell script to download data
+
+from astroquery.mast import Observations
+import os
+import numpy as np
+import pandas as pd
+import argparse
+from astropy.io import ascii
+import glob
+import sys
+import shutil
+import astropy.units as u
+import astropy.table
+from astropy.coordinates import SkyCoord
+from astropy.table import vstack
+import time
+
+Observations.enable_cloud_dataset(provider='AWS')
+
+parser = argparse.ArgumentParser(description='Download from MAST')
+
+## Set up input args to run from command line
+parser.add_argument('--RA', type=float, nargs="?",
+                    help='Central right ascension of catalogue (deg)', default=110.8375)
+parser.add_argument('--DEC', type=float, nargs="?", help="Central declination of catalogue (deg)", default=-73.4391)
+parser.add_argument('--RAD', type=float, nargs="?", help="Search radius (deg)", default=1.0)
+
+parser.add_argument('--TELESCOPE', type=str, nargs="*", help="Telescope to download")
+parser.add_argument('--CAMERA', type=str, nargs="*", help="Camera to download", default = ["ALL"])
+parser.add_argument('--FILTER', type=str, nargs="*", help="Filter to download", default = ["ALL"])
+
+parser.add_argument('--STAGE', type=str, nargs="*", default = "UNCAL",
+                    help='Product level (e.g., UN/CAL for un/cal files)')
+
+parser.add_argument('--VISITID', type=int, nargs="*", help="VISIT ID (e.g., 2736 for SMACS)", default = None)
+
+
+parser.add_argument('--dry_run', type=bool, nargs=1,
+                    help='Only download info table.')
+
+
+args = parser.parse_args()
+
+def cone_query(ref_dir, ra, dec, rad, telescope, camera, filter, stage, dl_products):
+    print("Querying observation")
+
+    ## Set up the search cone
+    coord = SkyCoord(ra=ra, dec=dec, unit=(u.degree, u.degree), frame="icrs")
+
+    ## convenience stub name for saving the astropy tables
+    stub_name = str(round(ra, 2)) + "_" + str(round(dec, 2)) + "_" + str(round(rad, 2)) + \
+                "_" + telescope + "_" + camera + "_" + filter + "_" + stage
+
+    ## where we will put the downloaded frames
+    dl_dir = os.path.join(ref_dir, telescope,
+                          str(round(ra, 2)) + "_" + str(round(dec, 2)) + "_" +
+                          str(round(rad, 2)),
+                          stage) + str("/")
+
+    ## Make sure that we get all HST frames that are part of HAP or HLA products
+    telescopes = [telescope]
+    if "HST" in telescope:
+        telescopes.append("HAP")
+        telescopes.append("HLA")
+
+    ## Now the meat of the code. Query MAST.
+    if camera == filter == "ALL":
+        obs_table = Observations.query_criteria(coordinates=coord,
+                                                radius=rad * u.degree,
+                                                project=telescopes,
+                                                dataproduct_type=["IMAGE", "image"]
+                                                )
+    elif filter == "ALL" and camera != "ALL":
+        obs_table = Observations.query_criteria(coordinates=coord,
+                                        radius=rad * u.degree,
+                                        project=telescopes,
+                                        dataproduct_type=["IMAGE", "image"],
+                                        instrument_name=[camera, camera + "/IMAGE"])
+    elif camera == "ALL" and filter != "ALL":
+        obs_table = Observations.query_criteria(coordinates=coord,
+                                                radius=rad * u.degree,
+                                                project=telescopes,
+                                                dataproduct_type=["IMAGE", "image"],
+                                                filters = filter)
+    else:
+        obs_table = Observations.query_criteria(coordinates=coord,
+                                                radius=rad * u.degree,
+                                                project=telescopes,
+                                                dataproduct_type=["IMAGE", "image"],
+                                                instrument_name=[camera, camera + "/IMAGE"],
+                                                filters = filter)
+
+
+    if len(obs_table) == 0:
+        pass
+    else:
+        # Observations.enable_cloud_dataset(provider='AWS')
+        product_list = [Observations.get_product_list(obs) for obs in obs_table]
+        data_products = vstack(product_list)
+
+        products_stage2 = Observations.filter_products(data_products,
+                                                       extension="fits",
+                                                       productSubGroupDescription=stage
+                                                       )
+
+        if "HAP-SVM" in set(products_stage2["project"]):
+            products_stage2 = products_stage2[products_stage2["project"] == "HAP-SVM"]
+
+        if len(products_stage2) == 0:
+            pass
+        else:
+
+            os.makedirs(dl_dir, exist_ok=True)
+
+            # sh_files = glob.glob(dl_dir + "*.sh")
+            # for file in sh_files:
+            #     os.remove(file)
+            # csv_files = glob.glob(dl_dir + "*.csv")
+            # for file in csv_files:
+            #     os.remove(file)
+
+            ascii.write(obs_table, dl_dir + stub_name +
+                        '_obs_table.csv', overwrite=True, format='csv')
+            ascii.write(products_stage2, dl_dir + stub_name +
+                        '_info.csv', overwrite=True, format='csv')
+
+            if dl_products:
+                Observations.download_products(products_stage2,
+                                              download_dir=dl_dir,
+                                              curl_flag=False,
+                                              cache=True)
+            return obs_table
+
+def vid_query(ref_dir, visit_id, stage, dl_products=False):
+    pid = str(visit_id)[0:4]
+
+    ## where we will put the downloaded frames
+    dl_dir = os.path.join(ref_dir, pid,
+                          stage) + str("/")
+    
+    os.makedirs(dl_dir, exist_ok=True)
+
+    print("Querying observation")
+    obs_table = Observations.query_criteria(proposal_id=pid,
+                                            project='JWST',
+                                            dataproduct_type = ["IMAGE", "image"]
+                                            )
+
+    product_list = [Observations.get_product_list(obs) for obs in obs_table]
+    data_products = vstack(product_list)
+    products_stage2 = Observations.filter_products(data_products,
+                                                   extension="fits",
+                                                   productSubGroupDescription=stage,
+                                                   )
+
+    visit_ids = np.array([obs[3:13] for obs in products_stage2["obs_id"]])
+    idx = np.flatnonzero(np.core.defchararray.find(visit_ids,str(visit_id))!=-1)
+
+    print("Moving the table")
+    Observations.download_products(products_stage2[idx], download_dir=dl_dir, curl_flag=True)
+    ascii.write(products_stage2, dl_dir + pid + '_' + stage + '.csv', overwrite=True, format='csv')
+    ascii.write(obs_table, dl_dir + pid + '_' + stage + '_obs_table.csv', overwrite=True, format='csv')
+
+    if dl_products:
+        Observations.download_products(products_stage2[idx],
+                                   download_dir=dl_dir,
+                                   curl_flag=False,
+                                   cache=True)
+
+    return obs_table
+
+def main(args_dict):
+
+    JUMPROPE_MAST_TOKEN = os.getenv('JUMPROPE_MAST_TOKEN')
+    JUMPROPE_DOWNLOAD_DIR = os.getenv('JUMPROPE_DOWNLOAD_DIR')
+
+    if None in [JUMPROPE_MAST_TOKEN, JUMPROPE_DOWNLOAD_DIR]:
+        print("Please set ENV variables. ")
+        exit()
+
+    my_session = Observations.login(token=str(JUMPROPE_MAST_TOKEN))
+    ref_dir = str(JUMPROPE_DOWNLOAD_DIR)
+
+    if args_dict["VISITID"] is not None:
+        args_grid = [(a, b) for a in args_dict["VISITID"]
+                            for b in args_dict["STAGE"]]
+        for i in range(len(args_grid)):
+            vid_query(
+                ref_dir = ref_dir,
+                visit_id = args_grid[i][0],
+                stage = args_grid[i][1],
+                dl_products = not args_dict["dry_run"]
+            )
+    else:
+        args_grid = [(a,b,c,d) for a in args_dict["TELESCOPE"]
+                               for b in args_dict["CAMERA"]
+                               for c in args_dict["FILTER"]
+                               for d in args_dict["STAGE"]]
+
+        for i in range(len(args_grid)):
+            cone_query(
+                ref_dir,
+                args_dict["RA"],
+                args_dict["DEC"],
+                args_dict["RAD"],
+                args_grid[i][0],
+                args_grid[i][1],
+                args_grid[i][2],
+                args_grid[i][3],
+                dl_products = not args_dict["dry_run"]
+            )
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    else:
+        args_dict = {
+            "RA" : args.RA,
+            "DEC" : args.DEC,
+            "RAD" : args.RAD,
+            "TELESCOPE" : args.TELESCOPE,
+            "CAMERA" : args.CAMERA,
+            "FILTER" : args.FILTER,
+            "STAGE" : args.STAGE,
+            "dry_run" : args.dry_run,
+            "VISITID" : args.VISITID
+        }
+
+        main(args_dict)
